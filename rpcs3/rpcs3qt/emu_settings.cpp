fbdd7135e0fe65c459b5ec5ba82d86fd3ab7ce39
tyler274 <tyler274port@gmail.com>
2017-07-07 17:07:08 -0700

    Revert "Mega dagi gui"

--- rpcs3/rpcs3qt/emu_settings.cpp
+++ rpcs3/rpcs3qt/emu_settings.cpp
@@ -207,47 +207,53 @@ void emu_settings::SaveSettings()
 	config.write(out.c_str(), out.size());
 }
 
-void emu_settings::EnhanceComboBox(QComboBox* combobox, SettingsType type)
+QComboBox* emu_settings::CreateEnhancedComboBox(SettingsType type, QWidget* parent)
 {
+	QComboBox* box = new QComboBox(parent);
+
 	for (QString setting : GetSettingOptions(type))
 	{
-		combobox->addItem(tr(setting.toStdString().c_str()), QVariant(setting));
+		box->addItem(tr(setting.toStdString().c_str()), QVariant(setting));
 	}
 
 	QString selected = qstr(GetSetting(type));
-	int index = combobox->findData(selected);
+	int index = box->findData(selected);
 	if (index == -1)
 	{
 		LOG_WARNING(GENERAL, "Current setting not found while creating combobox");
 	}
 	else
 	{
-		combobox->setCurrentIndex(index);
+		box->setCurrentIndex(index);
 	}
 
-	connect(combobox, static_cast<void(QComboBox::*)(int)>(&QComboBox::currentIndexChanged), [=](int index) {
-		SetSetting(type, sstr(combobox->itemData(index)));
+	connect(box, static_cast<void(QComboBox::*)(int)>(&QComboBox::currentIndexChanged), [=](int index) {
+		SetSetting(type, sstr(box->itemData(index)));
 	});
+
+	return box;
 }
 
-void emu_settings::EnhanceCheckBox(QCheckBox* checkbox, SettingsType type)
+QCheckBox* emu_settings::CreateEnhancedCheckBox(SettingsType type, QWidget* parent)
 {
 	cfg_location loc = SettingsLoc[type];
-	std::string name = loc[loc.size() - 1];
+	std::string name = loc[loc.size()-1];
+	QCheckBox* settingsButton = new QCheckBox(tr(name.c_str()), parent);
 
 	std::string currSet = GetSetting(type);
 	if (currSet == "true")
 	{
-		checkbox->setChecked(true);
+		settingsButton->setChecked(true);
 	}
 	else if (currSet != "false")
 	{
 		LOG_WARNING(GENERAL, "Passed in an invalid setting for creating enhanced checkbox");
 	}
-	connect(checkbox, &QCheckBox::stateChanged, [=](int val) {
+	connect(settingsButton, &QCheckBox::stateChanged, [=](int val) {
 		std::string str = val != 0 ? "true" : "false";
 		SetSetting(type, str);
 	});
+	return settingsButton;
 }
 
 std::vector<std::string> emu_settings::GetLoadedLibraries()
--- rpcs3/rpcs3qt/welcome_dialog.h
+++ rpcs3/rpcs3qt/welcome_dialog.h
@@ -4,17 +4,10 @@
 #include <QKeyEvent>
 #include <QCloseEvent>
 
-namespace Ui {
-	class welcome_dialog;
-}
-
 class welcome_dialog : public QDialog
 {
 	Q_OBJECT
 
 public:
 	explicit welcome_dialog(QWidget* parent = nullptr);
-
-private:
-	Ui::welcome_dialog *ui;
 };
